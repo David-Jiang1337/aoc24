239c95b91ab9136d115292c7378a18d38a75662c
David <david.jiang1337@gmail.com>
2025-08-09 13:59:40 -0700

    changed is_increasing and is_decreasing to use genericized function to reduce code duplication.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,18 +8,32 @@ fn int_vec_from_str(s: &str) -> Vec<i32> {
         .collect()
 }
 
-fn is_increasing(ints: &Vec<i32>) -> bool {
+fn is_ordered(ints: &Vec<i32>, compare: fn(i32, i32) -> bool) -> bool {
     for (i, val) in ints.iter().skip(1).enumerate() {
-        if ints[i - 1] >= *val {
+        if !compare(ints[i - 1], *val){
             return false;
         }
     }
     true
 }
 
+fn is_increasing(ints: &Vec<i32>) -> bool {
+    is_ordered(ints, |a: i32, b: i32| a < b)
+}
+
+fn is_decreasing(ints: &Vec<i32>) -> bool {
+    is_ordered(ints, |a: i32, b: i32| a > b)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
- 
+    
+    // ensures correct number of args
+    if args.len() != 2 {
+        println!("usage: {} <filepath>", args[0]);
+        return;
+    }
+
     let file_to_read = fs::read_to_string(&args[1])
         .expect("oops we couldn't get that file.");
 

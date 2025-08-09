371f1bd19a2943e1bafa6c60ecf65c8559fca514
David <david.jiang1337@gmail.com>
2025-08-09 11:52:48 -0700

    initialized main with template for input

---
+++ day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+/home/david/Documents/Projects/aoc24/day2/target/debug/day2: /home/david/Documents/Projects/aoc24/day2/src/main.rs
---
+++ day2/target/debug/deps/day2-1194884e9b2356ee.d
@@ -0,0 +1,5 @@
+/home/david/Documents/Projects/aoc24/day2/target/debug/deps/day2-1194884e9b2356ee.d: src/main.rs
+
+/home/david/Documents/Projects/aoc24/day2/target/debug/deps/day2-1194884e9b2356ee: src/main.rs
+
+src/main.rs:

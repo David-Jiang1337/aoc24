c87e8a616d89183e0ee14402132290b46055fcb3
David <david.jiang1337@gmail.com>
2025-08-09 19:57:10 -0700

    changed signature and effect of is_ordered

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -12,21 +12,21 @@ fn int_vec_from_str(s: &str) -> Vec<i32> {
         .collect()
 }
 /*
- * returns true if every element in the vector fulfills the comparison condition
- * with the next element in the vector, or if the list is empty or has only one
- * element. otherwise, returns false.
+ * if each consecutive pair of elements in ints satisifies the compare function,
+ * returns -1, otherwise, returns the index of the first element in the element pair
+ * that violates the condition.
  */
-fn is_ordered(ints: &Vec<i32>, tolerance: i32, compare: fn(i32, i32) -> bool) -> bool {
+fn is_ordered(ints: &Vec<i32>, tolerance: i32, compare: fn(i32, i32) -> bool) -> isize {
     let mut fails = 0;
     for (i, val) in ints.iter().skip(1).enumerate() {
         if !compare(ints[i], *val){
             fails += 1;
             if fails > tolerance {
-                return false;
+                return i.into();
             }
         }
     }
-    true
+    return -1;
 }
 
 /*
@@ -63,7 +63,7 @@ fn main() {
     
     let mut safe_count: u32 = 0;
 
-    for line in reports {
+    for line in reports.iter() {
         let levels: Vec<i32> = int_vec_from_str(line);
         if is_ordered(&levels, 0, greater_by_a_little)
             || is_ordered(&levels, 0, smaller_by_a_little) {
@@ -71,5 +71,18 @@ fn main() {
         }
     }
 
-    println!("safe reports: {safe_count}");
+    println!("safe reports w/o problem dampener: {safe_count}");
+
+
+    let mut safe_count: u32 = 0;
+
+    for line in reports.iter() {
+        let levels: Vec<i32> = int_vec_from_str(line);
+        if is_ordered(&levels, 1, greater_by_a_little)
+            || is_ordered(&levels, 1, smaller_by_a_little) {
+                safe_count += 1;
+        }
+    }
+
+    println!("safe reports w/ problem dampener: {safe_count}");
 }

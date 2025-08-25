a3106300c1dbe399fc1cb794abe6c2e56ecf4bb4
David <david.jiang1337@gmail.com>
2025-08-25 16:14:04 -0700

    added constants and signature to helper function

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,9 +1,24 @@
 use std::env;
 use std::fs;
+use regex::Regex;
+
+static ref MUL_PATTERN: Regex = Regex::new(r"mul\(\d+,\d+\)");
+static ref NUM_PATTERN: Regex = Regex::new(r"\d+");
+
+/*
+ * if the string is in the format "mul(<int>,<int>)", return the product
+ * of the two numbers. otherwise, panic
+ */
+fn calc_mul(string: &str) -> i32 {
+    
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
  
     let file_to_read = fs::read_to_string(&args[1])
         .expect("oops we couldn't get that file.");
+    
+    
+
 }

dc7792a16135cb5745d93188796e55e7e0f48c50
David <david.jiang1337@gmail.com>
2025-08-25 16:42:29 -0700

    implemented helper function calc_mul

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,16 +1,27 @@
 use std::env;
 use std::fs;
 use regex::Regex;
-
-static ref MUL_PATTERN: Regex = Regex::new(r"mul\(\d+,\d+\)");
-static ref NUM_PATTERN: Regex = Regex::new(r"\d+");
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref MUL_PATTERN: Regex = Regex::new(r"mul\(\d+,\d+\)").expect("whoops the regex pattern isn't valid");
+    static ref NUM_PATTERN: Regex = Regex::new(r"\d+").expect("whoops the regex pattern isn't valid");
+}
 
 /*
  * if the string is in the format "mul(<int>,<int>)", return the product
  * of the two numbers
  */
 fn calc_mul(string: &str) -> i32 {
-    
+    let match_nums: Vec<i32> = NUM_PATTERN.find_iter(string)
+        .map(|m| m.as_str().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let mut out: i32 = match_nums[0];
+    for m in match_nums.iter().skip(1) {
+        out *= m;
+    }
+
+    out
 }
 
 fn main() {
